78365bbce35c15d79dfea62cf3ec3942093121cb
TO <th.theophyl.hoguet@gmail.com>
2023-03-28 02:15:58 +0200

    projects finished need to add other projects to not overflow viewer

--- src/components/project.vue
+++ src/components/project.vue
@@ -1,24 +1,36 @@
 <template>
 	<div class="project">
-		<h1>{{ project.name }}</h1>
+		<div class="title">
+			<h2 class="year">{{ project.year }}</h2>
+			<a :href="project.links['title']">
+				<h1>{{ project.name }} <img class="icon" v-if="icon(project.type) != ''" :src="icon(project.type)" /></h1>
+			</a>
+			<h2 class="type">{{ project.type }}</h2>
+		</div>
 		<div class="info">
-			<p>{{ project.desc }}</p>
-			<a :href="project.links['title']" target="_blank">
+			<div class="descTechLinksLandscape">
+				<p class="desc">{{ project.desc }}</p>
+				<techAndLinks class="landscape" :project="project" :portrait="false" />
+			</div>
+			<a :href="project.links['title']" target="_blank" class="videoPreview">
 				<!-- autoplay  -->
 				<video autoplay loop muted>
 					<source :src="project.video" type="video/mp4" />
 				</video>
 			</a>
 		</div>
+		<techAndLinks class="portrait" :project="project" :portrait="true" />
 	</div>
 </template>
 
 <script setup lang="ts">
 import { defineComponent } from 'vue';
 import type { Project } from '@/assets/ts/project';
+import techAndLinks from './techAndLinks.vue';
 </script>
 
 <script lang="ts">
+
 
 export default defineComponent({
 	name: 'project',
@@ -50,6 +62,18 @@ export default defineComponent({
 			}
 		},
 	},
+	methods: {
+		icon(input: string) {
+			switch (input) {
+				case 'School Project':
+					return 'src/assets/images/school.svg';
+				case 'Personal Project':
+					return 'src/assets/images/perso.svg';
+				default:
+					return '';
+			}
+		}
+	}
 });
 
 </script>
@@ -59,6 +83,7 @@ export default defineComponent({
 	width: 100vw;
 	height: v-bind(height);
 	background-color: v-bind(color);
+	padding: 1vw 1vh 1vw 1vh;
 	display: flex;
 	align-items: center;
 	justify-content: center;
@@ -66,32 +91,93 @@ export default defineComponent({
 	transition: width 0.75s, height 0.75s;
 }
 
-h1 {
-	color: black;
-	font-size: 2.5em;
+.descTechLinksLandscape {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	gap: min(2vh, 20px);
+	max-width: 33%;
+	max-height: 60%;
+	flex-grow: 10;
 }
 
-p {
+
+.title {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	width: 80%;
+}
+
+.title a {
+	text-decoration: none;
+}
+
+:deep(a:hover:not(.videoPreview)) {
+	filter: invert(100%);
+}
+
+.title h1 {
+	transition: color 0.5s;
+	color: black;
+	font-size: 2.5em;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	gap: 20px;
+}
+
+.title h2 {
+	color: black;
+	font-size: 1.5em;
+}
+
+:deep(.icon) {
+	max-width: 40px;
+	max-height: 40px;
+}
+
+.desc {
 	color: black;
 	font-size: 1.5em;
 	z-index: 10;
+	background-color: gray;
+	padding: max(1vw, 1vh);
+	border-radius: 15px;
+	text-align: center;
+	overflow-y: auto;
+	box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.5);
+	-ms-overflow-style: none;
+	scrollbar-width: none;
 }
 
-video {
-	width: 100%;
-	height: 100%;
+.desc::-webkit-scrollbar {
+	display: none;
 }
 
-a {
+.videoPreview video {
 	width: 100%;
 	height: 100%;
+	object-fit: cover;
+}
+
+.videoPreview {
+	max-width: 100%;
+	max-height: 100%;
 	border-radius: 15px;
 	overflow: hidden;
 	z-index: 1;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-shrink: 10;
 }
 
 .info {
-	width: 90%;
+	padding: 2vw;
+	max-width: 100%;
+	max-height: 65%;
 	font-family: montserrat_regular;
 	display: flex;
 	align-items: center;
@@ -99,33 +185,73 @@ a {
 	flex-direction: row;
 }
 
-.info p {
-	background-color: gray;
-	padding: 25px;
-	border-radius: 15px;
-}
 
 /* if the screen is wider than tall */
 @media screen and (orientation: landscape) {
-	.info p {
-		margin-right: -35px;
-		height: 90%;
-	}
-
 	.info {
 		flex-direction: row;
+	}
+
+	.descTechLinksLandscape {
+		min-height: 90%;
+		flex-basis: 50%;
+		margin-right: -35px;
+		flex-shrink: 3;
+	}
+
+	.desc {
+		width: 100%;
+	}
+
+	.portrait {
+		display: none;
 	}
 }
 
 /* if the screen is taller than wide */
 @media screen and (orientation: portrait) {
-	.info p {
-		margin-bottom: -35px;
-		width: 90%;
-	}
-
 	.info {
 		flex-direction: column;
+	}
+
+	.descTechLinksLandscape {
+		min-width: 90%;
+		margin-bottom: max(-2vh, -35px);
+	}
+
+	.title {
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.title .type {
+		order: -1;
+	}
+
+	.title .year {
+		order: -2;
+	}
+
+	.landscape {
+		display: none;
+	}
+
+
+	.desc {
+		max-width: 90%;
+	}
+}
+
+@media screen and (max-height: 600px) {
+	.desc {
+		font-size: 1em;
+	}
+}
+
+@media screen and (max-height: 400px) {
+	:deep(.icon) {
+		max-width: 15px;
+		max-height: 15px;
 	}
 }
 </style>
---
+++ src/components/techAndLinks.vue
@@ -0,0 +1,113 @@
+<template>
+	<div class="techLinks">
+		<div class="techs">
+			<a v-for="tech in project.tech" :href="getTechLink(tech)">{{ tech }}</a>
+		</div>
+		<div class="images">
+			<a class="tooltip" v-if="'github' in project.links" :href="project.links['github']" title="See the code"><img
+					src="src/assets/images/github.svg" alt="GitHub" class="icon" /></a>
+			<a class="tooltip" v-if="'release' in project.links" :href="project.links['release']"
+				title="Test the program"><img src="src/assets/images/check.svg" alt="Release" class="icon" /></a>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from 'vue';
+import { getTechLink, type Project } from '@/assets/ts/project';
+</script>
+<script lang="ts">
+export default defineComponent({
+	name: 'techAndLinks',
+	props: {
+		project: {
+			type: Object as () => Project,
+			required: true,
+		}
+	},
+});
+</script>
+
+<style scoped>
+.techLinks {
+	width: calc(100% - 35px);
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	align-self: flex-end;
+	margin-right: 35px;
+	padding: 0 1vw 0 1vw;
+	flex-wrap: wrap;
+	flex-direction: row;
+}
+
+.images {
+	gap: 1vw;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+
+.images img {
+	box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.5);
+	border-radius: 50%;
+	border: 1px solid black;
+}
+
+.techs {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	gap: 1vw;
+	flex-wrap: wrap;
+}
+
+.techs a {
+	color: black;
+	font-weight: bold;
+	z-index: 10;
+}
+
+.tooltip {
+	position: relative;
+	display: inline-block;
+	z-index: 10;
+}
+
+.tooltip:hover::before {
+	content: attr(title);
+	position: absolute;
+	top: 100%;
+	left: 50%;
+	transform: translateX(-50%);
+	padding: 5px;
+	background-color: #333;
+	color: #fff;
+	font-size: 14px;
+	white-space: nowrap;
+	z-index: 10;
+	border-radius: 15px;
+}
+
+@media (orientation: portrait) {
+	.techLinks {
+		flex-direction: column;
+		margin-right: 0;
+		gap: 1vh;
+		width: 100%;
+	}
+}
+
+@media (max-width: 1000px) {
+	.techLinks {
+		flex-direction: column;
+		gap: 1vh;
+	}
+}
+
+@media (max-height: 400px) {
+	.techLinks {
+		font-size: 0.8em;
+	}
+}
+</style>
